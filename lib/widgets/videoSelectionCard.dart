91c9305cafe89e82421e3faa237265d250582cb1
Spoidy666 <147528118+Spoidy666@users.noreply.github.com>
2026-01-13 23:01:25 +0530

    feat: Add video selection card widget for improved course navigation

---
+++ lib/widgets/videoSelectionCard.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:learningapp/widgets/customPrimaryText.dart';
+
+class Videoselectioncard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String imagelocation;
+  final VoidCallback onTap;
+  const Videoselectioncard({
+    super.key,
+    required this.onTap,
+    required this.title,
+    required this.subtitle,
+    required this.imagelocation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 199, 199, 199),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          height: 100,
+          width: double.infinity,
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+                child: Container(
+                  height: 70,
+                  width: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.green,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(imagelocation, fit: BoxFit.fill),
+                  ),
+                ),
+              ),
+              SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 8),
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    subtitle,
+                    style: TextStyle(fontSize: 14, color: Colors.black),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
