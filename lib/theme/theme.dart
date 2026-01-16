1e1771e737051e9a9ef902f622c07e4dabfcdb02
Spoidy666 <147528118+Spoidy666@users.noreply.github.com>
2026-01-16 20:28:40 +0530

    feat: Update theme colors, enhance profile page layout, and add admin dashboard components

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -4,17 +4,19 @@ ThemeData lightmode = ThemeData(
   brightness: Brightness.light,
   colorScheme: ColorScheme.light(
     surface: const Color.fromRGBO(247, 247, 247, 1.0),
-    primary: Color.fromARGB(255, 38, 0, 255),
-    secondary: Colors.orangeAccent,
-    tertiary: Colors.black,
+    primary: Colors.blue,
+    secondary: Colors.black,
+    tertiary: const Color.fromARGB(255, 234, 233, 233),
+    onPrimary: Colors.black,
   ),
 );
 ThemeData darkmode = ThemeData(
   brightness: Brightness.dark,
   colorScheme: ColorScheme.dark(
     surface: Colors.grey.shade800,
-    primary: Color.fromARGB(255, 38, 0, 255),
-    secondary: Colors.orangeAccent,
-    tertiary: Colors.white,
+    primary: Color.fromARGB(255, 88, 62, 234),
+    secondary: Colors.white,
+    tertiary: const Color.fromARGB(255, 79, 79, 79),
+    onPrimary: Colors.white,
   ),
 );
