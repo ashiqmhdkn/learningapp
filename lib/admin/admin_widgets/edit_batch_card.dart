212a613cb620306729d72738daeda18a95f9a1f9
ASHIQ MUHAMED KN <123819168+ashiqmhdkn@users.noreply.github.com>
2026-03-10 22:41:14 +0530

    ashiq trails in request mangement

---
+++ lib/admin/admin_widgets/edit_batch_card.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class EditBatchCard extends StatelessWidget {
+  final String title;
+  final String image;
+  final VoidCallback onTap;
+  final VoidCallback onEdit;
+  final VoidCallback onDelete;
+  final VoidCallback onGenerate;
+
+  const EditBatchCard({
+    super.key,
+    required this.title,
+    required this.image,
+    required this.onTap,
+    required this.onEdit,
+    required this.onDelete,
+    required this.onGenerate
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.tertiary,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Stack(
+                  children: [
+                    Positioned.fill(
+                      child: Image.network(
+                        image,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+
+                    // Edit & Delete buttons
+                    Positioned(
+                      top: 8,
+                      right: 8,
+                      child: Row(
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.edit, color: Colors.green),
+                            onPressed: onEdit,
+                            splashRadius: 20,
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.red),
+                            onPressed: onDelete,
+                            splashRadius: 20,
+                          ),
+                           IconButton(
+                            icon: const Icon(Icons.sports_handball, color: Colors.blue),
+                            onPressed: onGenerate,
+                            splashRadius: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Text(
+                  title,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
